556f159aac7dace723b9c8300e3bcd445c23a65c
Jeremy McWilliams <jeremym@lclark.edu>
2020-08-09 17:22:36 -0700

    retrieve scope parameters

--- custom.js
+++ custom.js
@@ -10,12 +10,31 @@ angular.module('paginator', []).component('prmSearchResultListAfter', {
   bindings: {parentCtrl: '<' },
   controller: function controller($scope, $element){
     this.$onInit = function () {
+      console.log("paginator:");
+      console.log($scope);
+      var currentUrl=$scope.$parent.$ctrl.briefResultService.$location.$$absUrl;
+      var service=$scope.$parent.$ctrl.briefResultService;
 
+      var ro=$scope.$parent.$ctrl.briefResultService.searchStateService.resultObject;
+      const urlParams = new URLSearchParams(currentUrl);
 
+      var getTotals = setInterval(function() {
+
+         if (ro.info.total) {
+           $scope.totalResults=ro.info.total;
+           console.log($scope.totalResults)
+           console.log(ro.info.last)
+           var offset=parseInt(urlParams.get('offset'))
+           console.log(offset)
+
+            clearInterval(getTotals);
+
+         }
+      }, 100);
 
     }
   },
-  template: '<p>hello!</p>'
+  template: '<p><a href="">next page</a></p>'
 
 
 
